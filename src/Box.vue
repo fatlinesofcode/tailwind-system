53ea621a7f3a785901fbd527d5a5bfe9282a11fc
Phil Andrews <phil@frontside.com.au>
2020-11-12 16:05:47 +1100

    Box component

---
+++ src/Box.vue
@@ -0,0 +1,25 @@
+<template functional>
+  <div :class="$options.propsToClasses(data, props)" :style="data.style">
+    <slot />
+  </div>
+</template>
+
+<script>
+import propTypes from './propTypes'
+import { propsToClasses } from './propsToClasses'
+
+export default {
+  name: 'Box',
+  props: {
+    style: {},
+    ...propTypes.box
+  },
+  propsToClasses(data, props) {
+    const mapKeys = {
+      'text-align': 'text',
+      display: ''
+    }
+    return propsToClasses(data, props, { mapKeys })
+  }
+}
+</script>
