ac74a6daacb024cef9cbf8432a12ef53e76db675
Phil Andrews <phil@frontside.com.au>
2020-11-17 14:14:09 +1100

    vue3 example

---
+++ example/tailwind-system-example-vue3/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Box :pt="[1,2,3]" bg="red">box</Box>
+</template>
+
+<script>
+import Box from './components/Box.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Box
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ example/tailwind-system-example-vue3/src/components/Box.vue
@@ -0,0 +1,29 @@
+<template>
+  <component :is="tag" :class="classNames">
+    <slot />
+  </component>
+</template>
+
+<script>
+import { ref, computed } from "vue";
+import {propsToClasses, vuePropTypes} from "../../../../dist"
+const classNameProps = vuePropTypes.box
+export default {
+  name: 'Box',
+  props: {
+    tag: { type: String, default: 'div' },
+    ...classNameProps
+  },
+  setup(props){
+    const mapKeys = {
+      'text-align': 'text',
+      display: ''
+    }
+    const classNames = computed( () => propsToClasses(props, classNameProps, { mapKeys }))
+    return {
+      tag : props.tag || 'div',
+      classNames
+    }
+  }
+}
+</script>
